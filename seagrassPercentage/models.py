307588caf1196a1e466792a6f711e4751f59d1e5
savvyguru <pungtuckwei@gmail.com>
2020-05-29 17:57:03 +0800

    first commit

--- seagrassPercentage/models.py
+++ seagrassPercentage/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 def get_user_image_folder(instance, filename):
-    return "%s/%s" % (instance.user.user.username, filename)
+    return "%s/%s" % (instance.user.username, filename)
 
 #link user to images
 class Picture(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=get_user_image_folder)
+    image = models.ImageField(upload_to='media',default=None)
+    processedimage = models.ImageField(upload_to='media',default=None)
+    #image = models.ImageField(upload_to=get_user_image_folder)
+    #processedimage = models.ImageField(upload_to=get_user_image_folder)
--- seagrassPercentage/views.py
+++ seagrassPercentage/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import UploadFileForm
 from django.core.files.storage import FileSystemStorage
+from subprocess import Popen, PIPE, STDOUT
+from seagrassPercentage.models import User, Picture
 
 def handle_uploaded_file(f):
     with open('some/file/name.txt', 'wb+') as destination:
@@ -16,20 +18,23 @@ def Form(request):
 		form = UploadFileForm(request.POST, request.FILES)
 		if form.is_valid():
 			myfile = request.FILES['file']
-			fs = FileSystemStorage()
-			filename = fs.save(myfile.name, myfile)
-			uploaded_file_url = fs.url(filename)
+			#upload images to database
+			richard = User.objects.get(pk=1)
+			pic = Picture(user=richard,image=myfile,processedimage=myfile)
+			pic.save()
+			path = pic.processedimage.path		
+			#fs = FileSystemStorage()
+			#filename = fs.save(myfile.name, myfile)
+			#uploaded_file_url = fs.url(filename)
 			# call your python code for image processing
-			print('processing', uploaded_file_url)
-			
-			command = 'python /Users/richard/aglantisWebsite/seagrassPercentage/imgproc/seagrassCounter.py -i' + uploaded_file_url
-			os.system(command)
+			#print('processing', uploaded_file_url)
+			print('processing', path)
+			process = Popen(["python","/Users/richard/aglantisWebsite/seagrassPercentage/imgproc/seagrassCounter.py","-i",path])
+			#command = 'python /Users/richard/aglantisWebsite/seagrassPercentage/imgproc/seagrassCounter.py -i' + path
+			#os.system(command)
 			return HttpResponse("File(s) uploaded!")
 		else:
 			print('form invalid')
 			form = UploadFileForm()
             
 	return render(request, "seagrassPercentage/index.html",  {'form': form})
-
-
-
